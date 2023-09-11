8146a6895188f28a3dc7ae749b3a3796f2a189c0
julien <julienbraine@yahoo.fr>
2023-09-11 15:58:42 +0200

    update

--- src/progress_executor/sync_progress_executor.py
+++ src/progress_executor/sync_progress_executor.py
@@ -16,14 +16,7 @@ class SyncUpdater(Updater):
 class SyncProgressFuture(ProgressFuture):
 
     def __init__(self, f, args, kwargs):
-        # import inspect
-        # super(concurrent.futures.Future, self).__init__()
         super().__init__()
-        # print(inspect.getsource(concurrent.futures.Future.__init__))
-        # print(self.__dict__)
-        # input()
-        # print(self._condition)
-        # input()
         super()._child_init(progress_info=dict(n=0, total=0, status="pending"))
         self.f = f
         self.args= args
@@ -44,53 +37,15 @@ class SyncProgressFuture(ProgressFuture):
         except KeyboardInterrupt:
             signal.signal(signal.SIGINT, old_handler)
             old_handler(None, None)
-                    # self.executor.shutdown()
-            # self.set_exception( asyncio.CancelledError())
             self.cancel()
             progress.refresh()
-            # raise asyncio.CancelledError() from None
             raise
         except:
             signal.signal(signal.SIGINT, old_handler)
             raise
         self.set_result(res)
         progress.refresh()
-        return res
-        #     if not self.is_cancelled:
-        #         def mcheck_cancel():
-        #             if self.is_cancelled:
-        #                 raise asyncio.CancelledError() from None
-        #         progress = CustomUpdater(check_cancel=mcheck_cancel, on_progress=lambda n, tot: self._notify_progress(n, tot))
-        #         if not self.handlers is None:
-        #             def myhandler(*args, **kwargs):
-        #                 # print("HANDLER!!!")
-        #                 # time.sleep(10)
-        #                 self.handlers[0](*args, **kwargs)
-        #             signal.signal(signal.SIGINT, myhandler)
-            
-        #             # input("Changed handlers")
-        #             try:
-        #                 res = self.f(*self.args, check_cancel=mcheck_cancel, progress=progress, **self.kwargs)
-        #             except KeyboardInterrupt:
-        #                 signal.signal(signal.SIGINT, self.handlers[1])
-        #                 self.handlers[1](None, None)
-        #                 self.executor.shutdown()
-        #                 raise asyncio.CancelledError() from None
-        #             except:
-        #                 signal.signal(signal.SIGINT, self.handlers[1])
-        #                 raise
-        #         else:
-        #             res = self.f(*self.args, check_cancel=mcheck_cancel, progress=progress, **self.kwargs)
-        #             return res
-        #     else:
-        #         raise asyncio.CancelledError() from None
-        # except BaseException as e:
-        #     for c in self.done_callbacks:
-        #         c(e)
-        #     raise
-        # else:
-        #     for c in self.done_callbacks:
-        #         c(res)            
+        return res    
 
 class SyncProgressExecutor(ProgressExecutor):
     def __init__(self, *args, **kwargs):
--- tests/basic_run.py
+++ tests/basic_run.py
@@ -1,24 +1,10 @@
+
 import logging, beautifullogger
 import sys, time, asyncio, tqdm
 from progress_executor import *
 
 logger = logging.getLogger(__name__)
 
-def setup_nice_logging():
-    beautifullogger.setup(logmode="w")
-    logging.getLogger("toolbox.ressource_manager").setLevel(logging.WARNING)
-    logging.getLogger("toolbox.signal_analysis_toolbox").setLevel(logging.WARNING)
-
-def handle_exception(exc_type, exc_value, exc_traceback):
-    if issubclass(exc_type, KeyboardInterrupt):
-        logger.info("Keyboard interupt")
-        sys.exit()
-        return
-    else:
-        sys.__excepthook__(exc_type, exc_value, exc_traceback)
-
-
-sys.excepthook = handle_exception
 
 def long_compute(n):
     tot = 17
@@ -61,8 +47,8 @@ async def main():
             print(f"Task {i} with val={val} has result {'cancelled' if task.cancelled() else task.result()}")
     
 
-def run():
-    setup_nice_logging()
+if __name__ == "__main__":
+    beautifullogger.setup(warning_level_modules=["asyncio"])
     logger.info("Running start")
     asyncio.run(main())
     logger.info("Running end")
